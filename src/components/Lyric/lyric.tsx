851c49977434ab31df54f54feac556cc0b40b299
Ville Rahikka <ville.rahikka@nitor.com>
2023-09-01 11:43:45 +0300

    Add Vercel deploy and replace JSDOM

--- src/components/Lyric/lyric.tsx
+++ src/components/Lyric/lyric.tsx
@@ -1,47 +1,46 @@
+import * as cheerio from 'cheerio';
 import { component$ } from '@builder.io/qwik';
-import { JSDOM } from 'jsdom';
-import DOMPurify from 'dompurify';
 
 export interface LyricProps {
-  htmlText: string;
+  htmlString: string;
 }
 
-export const Lyric = component$<LyricProps>(({ htmlText }) => {
-  const { window } = new JSDOM('');
-  const purify = DOMPurify(window);
-  const sanitizedLyrics = purify
-    .sanitize(htmlText)
-    .replace(/\[(.*?)](?:<br>|<p>)/g, (match, p1) => {
-      let className = '';
-      if ((p1.toLowerCase() as string).includes('pre-chorus')) {
-        className = 'other special';
-      } else if ((p1.toLowerCase() as string).includes('chorus')) {
-        className = 'chorus special';
-      } else {
-        className = 'other special';
-      }
+export const Lyric = component$<LyricProps>(({ htmlString }) => {
+  const $ = cheerio.load(htmlString);
 
-      return `<p class="${className}">${match}</p>`;
-    })
-    .replace(/\[|\]/g, '');
-
-  const doc = new window.DOMParser().parseFromString(
-    sanitizedLyrics,
-    'text/html',
-  );
-
-  doc.querySelectorAll('a').forEach((anchor) => {
-    const p = doc.createElement('p');
-    p.innerHTML = anchor.innerHTML;
-    anchor.replaceWith(p);
+  $('a').each(() => {
+    const content = $(this).html(); // You can use .text() if you want just the text content
+    const pElement = $('<p></p>').html(content ?? '');
+    $(this).replaceWith(pElement);
   });
-  doc.querySelectorAll('i').forEach((anchor) => {
-    anchor.remove();
+
+  $('p').each(() => {
+    let textContent = $(this).html() ?? '';
+
+    textContent = textContent
+      .replace(/\[(.*?)](?:<br>|<p>)/g, (match, p1) => {
+        let className = '';
+        if (p1.toLowerCase().includes('pre-chorus')) {
+          className = 'other special';
+        } else if (p1.toLowerCase().includes('chorus')) {
+          className = 'chorus special';
+        } else {
+          className = 'other special';
+        }
+
+        return `<p class="${className}">${match}</p>`;
+      })
+      .replace(/\[|\]/g, '');
+
+    $(this).html(textContent);
   });
+
+  const newHtmlText = $.html();
+
   return (
     <div
       class="flex-col pb-[20rem] items-center text-center [&_.special]:underline [&_.special]:text-lightGray [&_.special]:mb-4 [&_.special]:decoration-2 [&_.chorus]:decoration-primary [&_.other]:decoration-secondary"
-      dangerouslySetInnerHTML={doc.body.innerHTML}
+      dangerouslySetInnerHTML={newHtmlText}
     />
   );
 });
